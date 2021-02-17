0dfbdce4fc47a3788e354199af502335dc2a9432
Soumitra <soumitra.banerjee@hotmail.com>
2021-02-17 13:05:19 +0530

    Adding extra features

--- TestProgram.java
+++ TestProgram.java
@@ -1,9 +1,18 @@
 import java.util.*;
+import java.io.*;
+
+class Test{
+  void animal(String str){
+    System.out.println("Tiger "+str);
+  }
+}
 
 public class TestProgram{
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     String str = sc.nextLine();
     System.out.println(str);
+    Test obj = new Test();
+    obj.animal(str);
   }
 }
