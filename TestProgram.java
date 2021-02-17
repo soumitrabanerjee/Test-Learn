f11228cc82f9d11c3b9db8eb2f661490837e092d
Soumitra Banerjee <46226374+soumitrabanerjee@users.noreply.github.com>
2021-02-17 11:49:07 +0530

    Added scanner class
    Input from user

--- TestProgram.java
+++ TestProgram.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class TestProgram{
   public static void main(String args[]){
-    System.out.println("Hello, World!");
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    System.out.println(str);
   }
 }
